096d8f1e8511dbfa5c79efdca7a3f85238ad56f1
Tinkler <tinkler@163.com>
2025-08-23 16:50:56 +0800

    fix: reuse price

--- pkg/models/vredis/pyth_price.go
+++ pkg/models/vredis/pyth_price.go
@@ -1,1 +1,25 @@
 package vredis
+
+import "encoding/json"
+
+func UnmarshalPrice(data []byte) (Price, error) {
+	var r Price
+	err := json.Unmarshal(data, &r)
+	return r, err
+}
+
+func (r *Price) Marshal() ([]byte, error) {
+	return json.Marshal(r)
+}
+
+// A geographical coordinate
+type Price struct {
+	Expo int64 `json:"expo"`
+	// pyth fee id
+	ID string `json:"id"`
+	// price with expo
+	Price       string `json:"price"`
+	PublishTime int64  `json:"publishTime"`
+	// price symbol ex. BTC/USD, ETH/USD
+	Symbol string `json:"symbol"`
+}
