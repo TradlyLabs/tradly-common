c1465d3860f0e44290ef15c8eb3b047f0b38c5ac
Tinkler <tinkler@163.com>
2025-10-25 21:21:42 +0800

    change struct

--- pkg/models/vredis/market_data.go
+++ pkg/models/vredis/market_data.go
@@ -12,12 +12,18 @@ var (
 
 // TokenUSDPrice represents market data from Redis Pub/Sub
 type TokenUSDPrice struct {
-	TokenID         string          `json:"tokenId"`
-	ChainID         json.Number     `json:"chainId"`
-	TokenAddress    string          `json:"tokenAddress"`
-	PairAddress     string          `json:"pairAddress"`
-	USDPrice        decimal.Decimal `json:"usdPrice"`
-	BlockTimestamp  int64           `json:"blockTimestamp"`
-	TransactionHash string          `json:"transactionHash"`
-	LogIndex        int64           `json:"logIndex"`
+	TokenID           string          `json:"tokenId"`
+	ChainID           json.Number     `json:"chainId"`
+	TokenAddress      string          `json:"tokenAddress"`
+	PairAddress       string          `json:"pairAddress"`
+	BlockTimestamp    int64           `json:"blockTimestamp"`
+	TransactionHash   string          `json:"transactionHash"`
+	LogIndex          int64           `json:"logIndex"`
+	PairTokenID       string          `json:"pairTokenId"`
+	DexID             string          `json:"dexId"`
+	Price             decimal.Decimal `json:"price"`
+	Timestamp         int64           `json:"timestamp"`
+	PairID            string          `json:"pairId"`
+	BaseTokenAddress  string          `json:"baseTokenAddress"`
+	QuoteTokenAddress string          `json:"quoteTokenAddress"`
 }
